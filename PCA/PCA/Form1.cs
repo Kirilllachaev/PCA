8ca75fb77ad35f8d972d129fb4d919e075ef78fd
Kirilllachaev <49283161+Kirilllachaev@users.noreply.github.com>
2021-03-10 00:49:42 +0300

    10 MArch

--- PCA/PCA/Form1.cs
+++ PCA/PCA/Form1.cs
@@ -15,9 +15,47 @@ namespace PCA
 		public Form1()
 		{
 			InitializeComponent();
+
+			//this.button1.Size = new System.Drawing.Size(87, 41);
+			//System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+			//path.AddEllipse(0, 0, 50, 40);// сам поэксперементируй.
+			//Region rgn = new Region(path);
+			//button1.Region = rgn;
+		//	button1.BackgroundImage = Image.FromFile(@"c:\Program Files\DAEMON Tools Toolbar\Resources\RadioDown.bmp");
+			//button1.Click += new EventHandler(button1_Click);
+
+			
+
 		}
 
 		private void button1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
 
 		}
